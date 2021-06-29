9741fd1be7f22e87f8d3d0b8f53a8812853b1da6
Marja Rolandsson <marja.rolandsson@gmail.com>
2021-06-29 11:42:47 +0200

    Added TodoSequencer class and tests

---
+++ TodoIt.Tests/TodoSequencerTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using TodoIt.Data;
+
+namespace TodoIt.Tests
+{
+    public class TodoSequencerTest
+    {
+        [Fact]
+        public void PersonIdZeroAfterReset()
+        {
+            TodoSequencer.Reset();
+
+            Assert.Equal(0, TodoSequencer.TodoId);
+        }
+
+    }
+}
---
+++ TodoIt/Data/TodoSequencer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoIt.Data
+{
+    public class TodoSequencer
+    {
+
+        private static int todoId;
+        public static int TodoId { get => todoId; set => todoId = value; }
+
+        public static int NextTodoId()
+        {
+            TodoId++;
+            return TodoId;
+        }
+
+        public static void Reset()
+        {
+            TodoId = 0;
+        }
+    }
+}
